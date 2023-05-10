603b851e8fdf37d81b8310fbaec01bedb31bf568
Vaibhav <vaibhavmahajan3003@gmail.com>
2023-05-10 15:31:16 +0530

    GroupAnagrams.js

---
+++ GroupAnagrams.js
@@ -0,0 +1,14 @@
+var groupAnagrams = function (strs) {
+  const map = new Map();
+
+  for (let str of strs) {
+    let sortedStr = str.split("").sort().join("");
+    if (map.has(sortedStr)) {
+      map.get(sortedStr).push(str);
+    } else {
+      map.set(sortedStr, [str]);
+    }
+  }
+
+  return Array.from(map.values());
+};
